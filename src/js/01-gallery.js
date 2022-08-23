d4e7367720a73c7f515195d5e13cadb052c4e966
Denis19850204 <102860941+Denis19850204@users.noreply.github.com>
2022-08-23 17:00:26 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const galleryCard = createGalleryItem(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryCard)
+
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+    
+    if (!event.target.classList.contains('gallery__image')) {
+        return
+    }
+    console.log(event.target)
+}
+
+function createGalleryItem(galleryItems) {
+  return galleryItems.map(({preview,original,description}) => {
+    return `<div class='gallery__item'>
+         <a class='gallery__link' href=${original}>
+         <img class='gallery__image'
+         src=${preview}
+         alt=${description}>
+        </a>
+        </div>`;
+  }).join('');   
+    
+}
+
+
+
